4b5d04dccc15bdece6ee07f10156aaef686f87a8
Juan David Castaño Gómez <juandcastag@gmail.com>
2022-09-29 21:36:24 -0500

    Landing_page_Monica

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,10 +3,9 @@ export const Services = (props) => {
     <div id='services' className='text-center'>
       <div className='container'>
         <div className='section-title'>
-          <h2>Our Services</h2>
+          <h2>Trabajos</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Algunas de las experiencias en mis trabajos
           </p>
         </div>
         <div className='row'>
